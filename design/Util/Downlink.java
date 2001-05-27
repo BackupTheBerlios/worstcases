d6be5f2c587e0197c2bb75211335c9ba7025fbc8
malte <malte>
2001-05-27 13:13:45 +0000

    Von System.out.println() auf Debug.println() umgestellt.

--- design/Util/Downlink.java
+++ design/Util/Downlink.java
@@ -4,6 +4,8 @@ import java.net.Socket;
 import java.util.Vector;
 import java.io.*;
 import Util.Commands.*;
+import Util.Debug.Debug;
+
 
 /**
  * Empfängt über einen Socket Nachrichten von einem Uplink und leitet sie an ihren Besitzer weiter.
@@ -12,89 +14,103 @@ import Util.Commands.*;
  * Diese Klasse wird z.B. vom ClientServant benutzt, um Nachrichten von seinem Client zu empfangen.
  */
 public class Downlink extends Thread {
-    private int LISTEN_DELAY = 1000;
 
-    /**
-     * Konstruktor.
-     * @param socket der zu benutzende Socket.
-     * @param owner der Besitzer des Downlinks.
-     */
-    public Downlink(Socket socket, DownlinkOwner owner) {
-        this.socket = socket;
-        this.setDownlinkOwner(owner);
-    }
+  private int LISTEN_DELAY = 1000;
 
-    /** Über diesen Socket werden die Nachrichten empfangen. */
-    private Socket socket;
+  /**
+   * Konstruktor.
+   * @param socket der zu benutzende Socket.
+   * @param owner der Besitzer des Downlinks.
+   */
+  public Downlink(Socket socket, DownlinkOwner owner) {
 
-    /** Der Besitzer des Downlinks, an den die ankommenden Nachrichten weitergeleitet werden sollen. */
-    private DownlinkOwner downlinkOwner;
-    private ObjectInputStream objectInputStream;
-    private boolean stop = false;
+    this.socket = socket;
 
-    /** Wartet auf ankommende Nachrichten und leitet sie an den Besitzer weiter. */
-    private void listen() {
-        //  String tmpString;
-        Command tmpCommand;
-        while (!stop) {
-            try {
-                tmpCommand = (Command)objectInputStream.readObject();
-                System.out.println("received " + tmpCommand);
-                downlinkOwner.processMsg(tmpCommand);
-                try {
-                    this.sleep(this.LISTEN_DELAY);
-                }
-                catch (java.lang.InterruptedException e) {
-                    System.out.println(e);
-                }
-            } catch (java.io.IOException e) {
-                System.out.println(e);
-                this.stopDownlink();
-            }
-            catch (java.lang.ClassNotFoundException e) {
-                System.out.println(e);
-            }
-        }
+    setDownlinkOwner(owner);
+  }
+
+  /** Über diesen Socket werden die Nachrichten empfangen. */
+  private Socket socket;
+
+  /** Der Besitzer des Downlinks, an den die ankommenden Nachrichten weitergeleitet werden sollen. */
+  private DownlinkOwner downlinkOwner;
+  private ObjectInputStream objectInputStream;
+  private boolean stop = false;
+
+  /** Wartet auf ankommende Nachrichten und leitet sie an den Besitzer weiter. */
+  private void listen() {
+
+    // String tmpString;
+    Command tmpCommand;
+
+    while (!stop) {
+      try {
+        tmpCommand = (Command) objectInputStream.readObject();
+
+        Debug.println("Downlink: received " + tmpCommand);
+        downlinkOwner.processMsg(tmpCommand);
+
         try {
-            this.objectInputStream.close();
+          sleep(Downlink.LISTEN_DELAY);
+        } catch (java.lang.InterruptedException e) {
+          Debug.println(Debug.HIGH, e);
         }
-        catch (java.io.IOException e) {
-            System.out.println(e);
-        }
-        System.out.println("downlink stopped");
+      } catch (java.io.IOException e) {
+        Debug.println(Debug.HIGH, e);
+        stopDownlink();
+      } catch (java.lang.ClassNotFoundException e) {
+        Debug.println(Debug.HIGH, e);
+      }
     }
 
-    /** Öffnet den Input - Stream, danach ist der Downlink betriebsbereit. */
-    public void startDownlink() throws java.io.IOException{
-            this.objectInputStream = new ObjectInputStream(this.socket.getInputStream());
-            System.out.println("downlink started");
+    try {
+      this.objectInputStream.close();
+    } catch (java.io.IOException e) {
+      Debug.println(Debug.HIGH, e);
     }
 
-    /** Startet den Thread. */
-    public void run() {
-        listen();
-    }
+    Debug.println("Downlink: stopped");
+  }
 
-    /** Schließt den Input - Stream */
-    private void stopDownlink() {
-        this.stop = true;
-    }
+  /** Öffnet den Input - Stream, danach ist der Downlink betriebsbereit. */
+  public void startDownlink() throws java.io.IOException {
 
-    public DownlinkOwner getDownlinkOwner() {
-        return downlinkOwner;
-    }
+    objectInputStream =
+      new ObjectInputStream(socket.getInputStream());
 
-    public void setDownlinkOwner(DownlinkOwner paramDownlinkOwner) {
-        if (this.downlinkOwner != paramDownlinkOwner) {
-            if (this.downlinkOwner != null) {
-                DownlinkOwner old = this.downlinkOwner;
-                this.downlinkOwner = null;
-                old.setDownlink(null);
-            }
-            this.downlinkOwner = paramDownlinkOwner;
-            if (paramDownlinkOwner != null) {
-                paramDownlinkOwner.setDownlink(this);
-            }
-        }
+    Debug.println("Downlink: started");
+  }
+
+  /** Startet den Thread. */
+  public void run() {
+    listen();
+  }
+
+  /** Schließt den Input - Stream */
+  private void stopDownlink() {
+    stop = true;
+  }
+
+  public DownlinkOwner getDownlinkOwner() {
+    return downlinkOwner;
+  }
+
+  public void setDownlinkOwner(DownlinkOwner paramDownlinkOwner) {
+
+    if (downlinkOwner != paramDownlinkOwner) {
+      if (downlinkOwner != null) {
+        DownlinkOwner old = downlinkOwner;
+
+        downlinkOwner = null;
+
+        old.setDownlink(null);
+      }
+
+      this.downlinkOwner = paramDownlinkOwner;
+
+      if (paramDownlinkOwner != null) {
+        paramDownlinkOwner.setDownlink(this);
+      }
     }
+  }
 }
