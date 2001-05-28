89c39f07361bb569c75dc7b64e957e4132a99737
mauper <mauper>
2001-05-28 16:59:18 +0000

    neuer, einheitlicher Datenbestand wird eingespielt-

--- design/Server/Servertest.java
+++
@@ -1,8 +0,0 @@
-package Server;
-
-public class Servertest {
-    public static void main(String[] args) throws java.io.FileNotFoundException, java.io.IOException {
-        Server tmpServer = new Server();
-        tmpServer.startServer();
-    }
-}
