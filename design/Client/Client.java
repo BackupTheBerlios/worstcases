54fff9b4df8317d730a952fcfd61e3a3601263fb
mauper <mauper>
2001-05-30 11:30:59 +0000

    an das Server - Design angepaßt, Kommentare verändert -

--- design/Client/Client.java
+++ design/Client/Client.java
@@ -8,161 +8,171 @@ import Util.Debug.Debug;
 
 /** Die Clientapplikation */
 public class Client implements Util.DownlinkOwner {
-
-
-    /** Betritt den angegebenen Channel. Das entpechende Command
-      * mit dem Channelnamen wid an den ClientSevant gesendet und
-      * dort mit den entsprechenden Methoden verarbeitet.
-      */
-    public void joinChannel(String name) {
+    /**
+     * Betritt den angegebenen Channel. Das entpechende Command mit dem Channelnamen wid an den ClientSevant gesendet und
+     * dort mit den entsprechenden Methoden verarbeitet.
+     * Benutzt JoinChannelCommand()
+     */
+    public synchronized final void joinChannel(String name) {
         this.sendCommand(
             new JoinChannelCommand(name));
     }
 
-    /** Wird aufgerufen, falls beim Betreten eines Channels ein Fehler
-      * auftritt und dieser an den Client gegeben wird. Der Benutzer
-      * befindet sich nicht in dem Channel und kann durch die GUI
-      * nicht interagieren.
-      */
-    public void joinChannelError(){
-    }
-
-    /** Dient dazu die Verbindung des Clients zum Serve zu trennen. Dazu
-      * werden die Referenzen auf das Uplink-, bzw. Downlinkobjekt ge-
-      * trennt und eine entsprechende Meldung wird auf dem GUI ausgege-
-      * ben.
-      */
-    public void stopClient(){
-    }
-
-    /** Wird gebraucht, um die Channels, die der Benutzer betreten darf
-      * auf der GUI auszugeben. Dazu werden die entsprechenden Daten
-      * via Downlink vom Server geholt und auf dem GUI ausgegeben.
-      */
-    public void setUserData(String userName,Vector channelNames){
-    }
-
-    /** Wird benötigt, um die Teilnehmer in dem Channel auf dem GUI
-      * anzuzeigen. Die Methode bekommt den Channelnamen und die Liste
-      * der Benutzer vom ClientServant durch den Downlink übergeben
-      * und gibt die Liste der Benutzer auf dem GUI aus.
-      */
-    public void setChannelData(String paramName,Vector userNames){
-    }
-
-
-    /** Sorgt dafür, daß im Hauptfenster die Meldung erscheint, daß ein
-      * User einen Channel betreten hat. Dazu wird der Name des neuen
-      * Benutzers an die Methode übergeben, die dann die Nachricht
-      * ausgibt.
-      */
-    public void newUserInChannel(String paramName) {
-        Debug.println("Client: " + paramName + " joined channel");
-    }
-
-    /** Erzeugt en Objekt Downlink für den zuständigen Client. Über den
-      * Downlink ist der Client in der Lage, Nachrichten vom Client-
-      * Servant zu empfangen.
-      */
-    public void setDownlink(Util.Downlink paramDownlink) { };
-
-    /** Wird aufgerufen, falls es Probleme mit dem Downlink gibt, bzw.
-      * kein Objekt dieser Klasse erzeugt werden kann.
-      */
-    public void downlinkError() { };
-
-
-
-    /** Wird aufgerufen, wenn vom ClientServant beim Einloggen, die
-      * Nachricht kommt, das das Einloggen nicht möglich ist. Die
-      * Methode sorgt dafür, das der Benutzer eine Fehlermeldung
-      * bekommt und zum Loginscreen zurückgelangt.
-      */
-    public void loginError() {
-        Debug.println("Client: login failed: ");
-    }
-
-    /** Wird aufgerufen, wenn vom ClientServant die Meldung kommt, das
-      * eine Nachricht an alle User im Channel nicht versendet werden
-      * konnte. Es wird eine entsprechende Fehlermeldung ausgegeben.
-      */
-    public void sendMsgToChannelError(){
-    }
-
-    /** Wird aufgerufen, wenn vom ClientServant die Meldung kommt, das
-      * eine Nachricht an einen anderen User im Channel nicht versendet
-      * werden konnte. Es wird eine entsprechende Fehlermeldung ausgegeben.
-      */
-   public void sendMsgToUserError(){
-    }
-
-   /** Dient dazu, eine Nachricht über das für den Client zuständige
-   	 * Uplinkobjekt an den ClientServant zu verschicken. Dabei wird
-     * die Nachricht direkt an den Uplink übergeben. Über diese
-     * Methode läuft alles, was der Client an den ClientServant
-     * sendet.
+    /**
+     * Wird aufgerufen, falls beim Betreten eines Channels ein Fehler
+     * auftritt und dieser an den Client gegeben wird. Der Benutzer
+     * befindet sich nicht in dem Channel und kann durch die GUI nicht interagieren.
+     * Der Client und seine GUI werden in den Zustand zurückversetzt, der nach dem Login
+     * des Users bestand.
      */
-    protected void sendCommand(Command paramCommand) {
+    public synchronized final  void joinChannelError() {
+    }
+
+    /**
+     * Dient dazu, die Verbindung des Clients zum Server zu trennen. Dazu
+     * werden die Referenzen auf das Uplink-, bzw. Downlinkobjekt ge-
+     * trennt und eine entsprechende Meldung wird auf dem GUI ausgegeben.
+     * Benutzt setDownlink(null) und uplink.stopUplink()
+     */
+    public  synchronized final void stopClient() {
+      this.setDownlink(null);
+      if(uplink!=null){
+       this.uplink.stopUplink();
+       this.uplink=null;
+
+      }
+
+    }
+
+    /**Der Name des Benutzers des Clients*/
+    String currentUserName;
+
+    /**Namensliste der Channels, die der Benutzer betreten darf*/
+    Vector currentAllowedChannelNames;
+
+    /**Der Name des Channels, der momentan betreten wurde*/
+    String currentChannelName;
+
+
+    /**
+     * Wird gebraucht, um die Channels, die der Benutzer betreten darf
+     * auf der GUI auszugeben. Dazu werden die entsprechenden Daten via Downlink vom Server geholt und auf dem GUI ausgegeben.
+     * Setzt currentUserName und currentAllowedChannelNames.
+     */
+    public  synchronized final void setCurrentUserData(String userName, Vector channelNames) {
+      this.currentUserName=userName;
+      this.currentAllowedChannelNames=channelNames;
+    }
+
+    /**
+     * Wird benötigt, um die Teilnehmer in dem Channel auf dem GUI
+     * anzuzeigen. Die Methode bekommt den Channelnamen und die Liste
+     * der Benutzer vom ClientServant durch den Downlink übergeben und gibt die Liste der Benutzer auf dem GUI aus.
+     * Setzt currentChannelName und currentUsersInChannelList
+     */
+    public synchronized final  void setCurrentChannelData(String paramName, Vector userNames) {
+      this.currentChannelName=paramName;
+      this.currentUsersInChannelList=userNames;
+
+    }
+
+    /**
+     * Über den
+     * Downlink ist der Client in der Lage, Nachrichten vom Client- Servant zu empfangen.
+     * Setzt downlink, benachrichtigt das Downlinkobjekt mittels setDownlinkOwner()
+     */
+    public synchronized final void setDownlink(Util.Downlink paramDownlink) { };
+
+    /** Wird aufgerufen, falls es Probleme mit dem Downlink gibt,
+      *  bzw. kein Objekt dieser Klasse erzeugt werden kann.
+      * Ruft stopClient() auf.
+      */
+    public synchronized final  void downlinkError() {
+     stopClient();
+    };
+
+    /**
+     * Wird aufgerufen, wenn vom ClientServant beim Einloggen, die Nachricht kommt, das das Einloggen nicht möglich ist. Die
+     * Methode sorgt dafür, das der Benutzer eine Fehlermeldung bekommt und zum Loginscreen zurückgelangt.
+     * Ruft stopClient() auf
+     */
+    public  synchronized final void loginError() {
+        Debug.println("Client: login failed: ");
+        stopClient();
+    }
+
+
+
+    /**
+     * Dient dazu, eine Nachricht über das für den Client zuständige
+     * Uplinkobjekt an den ClientServant zu verschicken. Dabei wird die Nachricht direkt an den Uplink übergeben. Über diese
+     * Methode läuft alles, was der Client an den ClientServant sendet.
+     * Benutzt uplink.sendMsg(). Bei einem Fehler wird stopClient() aufgerufen.
+     */
+    protected  synchronized final void sendCommand(Command paramCommand) {
         try {
             this.uplink.sendMsg(paramCommand);
         }
         catch (java.io.IOException e) {
             Debug.println(Debug.HIGH, e);
+            this.stopClient();
+
         }
+
     }
 
-    /** Wird aufgerufen, wenn ein Channel verlassen werden soll. Der
-      * Benutzer wird zur Auswahl eines neuen Channels aufgeordert.
-      */
-    public void leaveChannel() { }
+    /**
+     * Wird aufgerufen, wenn ein Channel verlassen werden soll. Der
+     * Benutzer wird zur Auswahl eines neuen Channels aufgeordert.
+     * Sendet ein LeaveChannelCommand().
+     */
+    public synchronized final  void leaveChannel() {
+      this.sendCommand(new LeaveChannelCommand());
 
-    /** Meldet den Benutzer an. Name und Passwort werden per Methode
-      * sendCommand an den ClientServant gesendet.
+        }
+
+    /** meldet den Benutzer an. Name und Passwort werden per Methode sendCommand an den ClientServant gesendet.
+      * Sendet LoginCommand().
       */
-    public void login(String name, String password) {
+    public synchronized final void login(String name, String password) {
         this.sendCommand(
             new LoginCommand(name, password));
     }
 
-    /** Meldet einen Gast an. Die Methode funktioniert wie die normale
-      * Methode login, jedoch wird kein Passwort erwartet.
+    /** meldet einen Gast an. Die Methode funktioniert wie die normale Methode login, jedoch wird kein Passwort erwartet.
+      * Sendet LoginGuestCommand().
       */
-    public void loginAsGuest(String name) {
+    public  synchronized final void loginAsGuest(String name) {
         Debug.println("Client: loginGuest " + name);
         this.sendCommand(
             new LoginGuestCommand(name));
     }
 
-    /** Meldet den Benutzer ab. Die Methode sorgt dafür, das der
-      * Benutzer zum Loginscreen zuückgelangt.
+    /** meldet den Benutzer ab. Die Methode sorgt dafür, das der Benutzer zum Loginscreen zuückgelangt.
+      * Sendet ein LogoutCommand()
       */
-    public void logout() { }
+    public synchronized final void logout() {
+      this.sendCommand(new LogoutCommand());
+
+    }
+
 
     /**
-     * Setzt availableChannels mit den Daten aus dem String channelSet.
-     * Der Client erhält die für den Benutzer zugänglichen Channels.
+     * sendet eine Nachricht an einen einzigen Benutzer. Die Nachricht, sowie
+     * der Name des Empfängrs (name) wird an die Methode übergeben, die sie dann via Uplink an den ClientServant sendet.
+     * Sendet sendMsgToUserCommand.
      */
-    public void setAvailableChannelList(String channelSet) { }
+    public synchronized final  void sendMsgToUser(String name, String msg) {
+     this.sendCommand(new SendMsgToUserCommand(name,msg));
+
+    }
 
     /**
-     * Setzt currentUsers mit den Daten aus dem String userSet.
-     * Der Client erhält die Liste der aktuellen Benutzer in dem betretenen Channel
+     * Sendet eine Nachricht in einen Channel, die von allen Teilnehmern
+     * in diesem Channel empfangen werden kann. Die Nachricht wird an die
+     * Methode übergeben und durch anlegen eines neuen Objekts SendMsgToChannel an den ClientServant gesendet.
      */
-    public void setCurrentUserInChannelList(String userSet) { }
-
-    /** Sendet eine Nachricht an einen einzigen Benutzer. Die Nachricht, sowie
-      * der Name des Empfängrs (name) wird an die Methode übergeben, die sie
-      * dann via Uplink an den ClientServant sendet.
-      */
-    public void sendMsgToUser(String name, String msg) { }
-
-
-    /** Sendet eine Nachricht in einen Channel, die von allen Teilnehmern
-      * in diesem Channel empfangen werden kann. Die Nachricht wird an die
-      * Methode übergeben und durch anlegen eines neuen Objekts SendMsgToChannel
-      * an den ClientServant gesendet.
-      */
-    public void sendMsgToChannel(String msg) {
+    public synchronized final void sendMsgToChannel(String msg) {
         this.sendCommand(
             new SendMsgToChannelCommand(msg));
     }
@@ -171,47 +181,48 @@ public class Client implements Util.DownlinkOwner {
      * Verarbeitet eine empfangene Nachricht bzw. führt den empfangenen Befehl einfach aus.
      * Nachrichten vom Server, die durch den Downlink empfangen werden, werden hier als Parameter eingesetzt.
      */
-    public void processMsg(Command msg) {
+    public synchronized final  void processMsg(Command msg) {
         msg.execute(this);
     }
 
-    /** Dient dazu, einen Client am Server anzumelden. Dazu wird für
-      * den neuen Client ein Socket am Server geöffnet, sowie ein
-      * Uplink Objekt und ein Downlink Objekt zur Kommunikation mit
-      * dem ClientServant.
-      */
-    public void startClient() {
+    /**
+     * Dient dazu, einen Client am Server anzumelden. Dazu wird für den neuen Client ein Socket am Server geöffnet, sowie ein
+     * Uplink Objekt und ein Downlink Objekt zur Kommunikation mit dem ClientServant.
+     * Benutzt setDownlink, downlink.startDownlink() und uplink.startUplink().
+     */
+    public synchronized final  void startClient() {
         Debug.println("Client: starting client");
         try {
-            socket = new Socket(Client.SERVER_IP, Client.SERVER_PORT);
+            socket = new Socket(SERVER_IP, SERVER_PORT);
             uplink = new Util.Uplink(socket);
-            downlink = new Util.Downlink(socket, this);
+            setDownlink (new Util.Downlink(socket, this));
             uplink.startUplink();
             downlink.startDownlink();
-            downlink.start();
         } catch (java.io.IOException e) {
             Debug.println(Debug.HIGH, e);
         }
     }
-    /** Dient dazu, eine Nachricht von einem einzigen Benutzer (fromName),
-      * die für den gesamten Channel bestimmt ist, zu empfangen. Die Nachricht
-      * wird vom Downlink an diese Methode übergeben und sie sorgt dafür, daß
-      * der empfangene Text in der GUI erscheint.
-      */
-    public void sendMsgFromChannel(String fromName, String msg) {
+
+    /**
+     * Dient dazu, eine Nachricht von einem einzigen Benutzer (fromName),
+     * die für den gesamten Channel bestimmt ist, zu empfangen. Die Nachricht
+     * wird vom Downlink an diese Methode übergeben und sie sorgt dafür, daß der empfangene Text in der GUI erscheint
+     * und zum channelMsgBuffer hinzugefügt wird.
+     */
+    public  synchronized final void sendMsgFromChannel(String fromName, String msg) {
         Debug.println("Client: " + fromName + " sayz:" + msg);
     }
 
-    /** Dient dazu, eine Nachricht von einem einzigen Benutzer (fromName)
-      * zu empfangen. Die Nachricht wird vom Downlink an diese Methode
-      * übergeben und sie sorgt dafür, daß der empfangene Text in der
-      * GUI erscheint.
-      */
-   public void sendMsgFromUser(String fromName,String msg){
+    /**
+     * Dient dazu, eine Nachricht von einem einzigen Benutzer (fromName)
+     * zu empfangen. Die Nachricht wird vom Downlink an diese Methode
+     * übergeben und sie sorgt dafür, daß der empfangene Text in der GUI erscheint
+     * und zum channelMsgBuffer hinzugefügt wird.
+     */
+    public  synchronized final void sendMsgFromUser(String fromName, String msg) {
     }
 
     /**
-     * Der Uplink, über ihn werden Nachrichten gesendet.
      * @directed
      * @clientCardinality 1
      * @supplierCardinality 1
@@ -219,39 +230,27 @@ public class Client implements Util.DownlinkOwner {
     protected Util.Uplink uplink;
 
     /**
-     * Der Downlink, über ihn werden Nachrichten empfangen.
      * @directed
      * @supplierCardinality 1
      * @clientCardinality 1
      */
     protected Util.Downlink downlink;
 
-    /** Der Port des Servers. */
-    protected final static int SERVER_PORT = 1500;
+    /** der Port des Servers */
+    protected int SERVER_PORT = 1500;
 
-    /** Die IP - Adresse des Servers. */
-    protected final static String SERVER_IP = "localhost";
+    /** die IP - Adresse des Servers */
+    protected String SERVER_IP = "localhost";
 
-    /** 
-      * Vector von Strings, repräsentiert die für den Benutzer freigegebenen
-      * Channels.
-      */
-     /* FIXME: Welche Methode?!
-      * Die Methode ist nötig, damit der Server erkennt, für welche Channels
-      * der Benutzer Zugriff hat.
-     */
-    protected Vector availableChannelList;
 
     /**
-     * Speichert ankommende Nachrichten. Wird vom GUI benutzt und kann als
+     * Speichert ankommende Nachrichten in einem Channel. Wird vom GUI benutzt und kann als
      * Protokoll der Unterhaltung in dem Channel dienen.
      */
     public String channelMsgBuffer = new String();
+
     protected Socket socket;
 
-    /** Vector von Strings, repräsentiert die aktuellen Benutzer in einem
-      * Channel.
-      * Wird vom GUI bennutzt.
-      */
-    protected Vector currentUserInChannelList;
+    /** Vector von Strings, repräsentiert die aktuellen Benutzer in einem Wird im Client Window eingebunden. */
+    protected Vector currentUsersInChannelList;
 }
