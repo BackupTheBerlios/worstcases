758c799160aa5240948359a2dbfba4114f3614e7
malte <malte>
2001-06-16 17:04:45 +0000

    Korrektur des Kommentars korrigiert ;-)

--- implementation/Util/Debug/Debug.java
+++ implementation/Util/Debug/Debug.java
@@ -2,7 +2,7 @@ package Util.Debug;
 
 /**
  * Klasse zur Ausgabe von Debug-Nachrichten. Gegenüber dem üblichen
- * <pre>Debug.println("Wichtiger Fehler!");</pre> 
+ * <pre>System.out.println("Wichtiger Fehler!");</pre> 
  * haben so ausgegebene Nachrichten u.a. folgende Vorteile:
  * <ul>
  * <li>sie können im Code bleiben, müssen nicht auskommentiert werden</li>
