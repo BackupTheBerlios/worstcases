81f76921938ad8823478b9417b44f02cc18c8bb2
malte <malte>
2001-06-16 16:39:42 +0000

    Fabians Bugfix wieder eingespielt (Jan hatte eine veraltete Version kommentiert).

--- implementation/Util/Commands/SetCurrentUserDataCommand.java
+++ implementation/Util/Commands/SetCurrentUserDataCommand.java
@@ -4,36 +4,36 @@ import Client.Client;
 import java.util.Vector;
 
 /**
-*Setzt bei einem Client Informationen über den aktuellen Benutzer. Funktioniert
-* mit der Methoder setCurrentUserData der Klasse Client.
-*/
+ * Setzt bei einem Client Informationen über den aktuellen Benutzer. Funktioniert
+ * mit der Methoder setCurrentUserData der Klasse Client.
+ */
 
 public class SetCurrentUserDataCommand implements Command {
-    /**
-    * Setzt folgende Attribute: Name des Benutzers(paramUserName), hat der Benutzer
-    * Administrationsrechte? (paramIsAdmin), Liste der Channels, die der Benutzer
-    * betreten darf (paramChannelNames).
-    */
-    public SetCurrentUserDataCommand(String paramUserName,boolean paramIsAdmin,Vector paramChannelNames) {
+  /**
+   * Setzt folgende Attribute: Name des Benutzers(paramUserName), hat der Benutzer
+   * Administrationsrechte? (paramIsAdmin), Liste der Channels, die der Benutzer
+   * betreten darf (paramChannelNames).
+   */
+    public SetCurrentUserDataCommand(String paramUserName,boolean paramIsAdmin,Vector paramChannelNames,String paramCurrentChannelName) {
         this.userName=paramUserName;
         this.isAdmin=paramIsAdmin;
         this.channelNames = paramChannelNames;
+        this.currentChannelName =paramCurrentChannelName;
     }
 
     /** Der Benutzername. */
     String userName;
-
- /** Ist der Benutzer Administrator? */
-
+    /**Adminstatus*/
     boolean isAdmin=false;
-
     /**Liste der Channelnamen, die der Benutzer betreten darf.*/
     Vector channelNames;
+    /**Name des momentan besuchten Channels*/
+    String currentChannelName;
 
     /**Ruft Client.setCurrentUserData auf.*/
     public void execute(Object target) {
         if (target instanceof Client) {
-            ((Client)target).setCurrentUserData(userName,isAdmin,channelNames);
-        }
+            ((Client)target).setCurrentUserData(userName,isAdmin,channelNames,currentChannelName);
+        } // XXX: else Exception auslösen?
     }
 }
