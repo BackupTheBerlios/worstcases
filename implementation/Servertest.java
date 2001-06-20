be756c05494e3d3ddf890a06d86aa62268751cd6
malte <malte>
2001-06-20 11:11:01 +0000

    Debug-Level wird vom Server bestimmt.

--- implementation/Servertest.java
+++ implementation/Servertest.java
@@ -1,7 +1,9 @@
 import Server.*;
+import Util.Debug.Debug;
 
 class Servertest{
  public static void main(String[] args){
+  Debug.setLevel(Debug.HIGH);
   Server testserver=new Server();
   testserver.startServer();
  
