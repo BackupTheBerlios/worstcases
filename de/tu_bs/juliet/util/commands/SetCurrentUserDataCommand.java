423fd4a2bfb0c31cb32f6fba3662da6659d814c2
malte <malte>
2001-06-20 22:47:42 +0000

    Package-Name korrigiert.

--- de/tu_bs/juliet/util/commands/SetCurrentUserDataCommand.java
+++ de/tu_bs/juliet/util/commands/SetCurrentUserDataCommand.java
@@ -21,7 +21,7 @@
    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
-package de.tu_bs.juliet.util.Commands;
+package de.tu_bs.juliet.util.commands;
 
 import Client.Client;
 import java.util.Vector;
